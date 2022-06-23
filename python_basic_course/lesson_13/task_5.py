64d196e1be5e26ec44c3d0e39f7aa0be8b39eac9
Artem0791 <artemveselov91@gmail.com>
2022-06-23 21:38:36 +0300

    complete 5 task

--- python_basic_course/lesson_13/task_5.py
+++ python_basic_course/lesson_13/task_5.py
@@ -23,34 +23,61 @@
 # чтобы в основной части программы был только ввод чисел, затем изменённые числа и вывод их суммы.
 
 first_n = int(input("Введите первое число: "))
-first_num_count = 0
-temp = first_n
+second_n = int(input("Введите второе число: "))
 
-while temp > 0:
-    first_num_count += 1
-    temp = temp // 10
-
-if first_num_count < 3:
-    print("В первом числе меньше трёх цифр.")
-else:
-    last_digit = first_n % 10
-    first_digit = first_n // 10 ** (first_num_count - 1)
-    between_digits = first_n % 10 ** (first_num_count - 1) // 10
-    first_n = last_digit * 10 ** (first_num_count - 1) + between_digits * 10 + first_digit
-    print('Изменённое первое число:', first_n)
-    second_n = int(input("\nВведите второе число: "))
-    second_num_count = 0
-    temp = second_n
-
+def num_count(temp):
+    cnt = 0
     while temp > 0:
-        second_num_count += 1
+        cnt += 1
         temp = temp // 10
-    if second_num_count < 4:
-        print("Во втором числе меньше четырёх цифр.")
+    return cnt
+
+def reverse_num(numb, count):
+    n_count = num_count(numb)
+    if n_count < count:
+        print("В числе меньше цифр.")
     else:
-        last_digit = second_n % 10
-        first_digit = second_n // 10 ** (second_num_count - 1)
-        between_digits = second_n % 10 ** (second_num_count - 1) // 10
-        second_n = last_digit * 10 ** (second_num_count - 1) + between_digits * 10 + first_digit
-        print('Изменённое второе число:', second_n)
-        print('\nСумма чисел:', first_n + second_n)
+        last_digit = numb % 10
+        first_digit = numb // 10 ** (n_count - 1)
+        between_digits = numb % 10 ** (n_count - 1) // 10
+        result = last_digit * 10 ** (n_count - 1) + between_digits * 10 + first_digit
+        return result
+
+first_n = reverse_num(first_n, 3)
+second_n = reverse_num(second_n, 4)
+print(first_n + second_n)
+
+
+# то, что было
+# first_n = int(input("Введите первое число: "))
+# first_num_count = 0
+# temp = first_n
+#
+# while temp > 0:
+#     first_num_count += 1
+#     temp = temp // 10
+#
+# if first_num_count < 3:
+#     print("В первом числе меньше трёх цифр.")
+# else:
+#     last_digit = first_n % 10
+#     first_digit = first_n // 10 ** (first_num_count - 1)
+#     between_digits = first_n % 10 ** (first_num_count - 1) // 10
+#     first_n = last_digit * 10 ** (first_num_count - 1) + between_digits * 10 + first_digit
+#     print('Изменённое первое число:', first_n)
+#     second_n = int(input("\nВведите второе число: "))
+#     second_num_count = 0
+#     temp = second_n
+#
+#     while temp > 0:
+#         second_num_count += 1
+#         temp = temp // 10
+#     if second_num_count < 4:
+#         print("Во втором числе меньше четырёх цифр.")
+#     else:
+#         last_digit = second_n % 10
+#         first_digit = second_n // 10 ** (second_num_count - 1)
+#         between_digits = second_n % 10 ** (second_num_count - 1) // 10
+#         second_n = last_digit * 10 ** (second_num_count - 1) + between_digits * 10 + first_digit
+#         print('Изменённое второе число:', second_n)
+#         print('\nСумма чисел:', first_n + second_n)
